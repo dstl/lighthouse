b74eb9fe3cfecdccbe5c6d7376789f685549ee3a
Michael Allen <michael@michaelallen.io>
2016-02-11 15:05:27 +0000

    Render users on the login page

--- apps/login/tests/test_login.py
+++ apps/login/tests/test_login.py
@@ -1,9 +1,26 @@
 from django.core.urlresolvers import reverse
 from django_webtest import WebTest
+from apps.users.models import User
 
 
 class LoginTest(WebTest):
 
+    def test_login_view_no_users(self):
+        response = self.app.get(reverse('login-view'))
+
+        self.assertEquals(0, len(response.context['users']))
+        self.assertEquals(response.status_int, 200)
+
     def test_login_view(self):
+        u1 = User(
+            fullName='John Smith',
+            phone='0 123 4567 890',
+            email='john.smith@smithmail.com',
+        )
+        u1.save()
+
         response = self.app.get(reverse('login-view'))
         self.assertEquals(response.status_int, 200)
+        self.assertIn(u1, response.context['users'])
+
+        self.assertIn('John Smith', response)
--- apps/login/views.py
+++ apps/login/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from apps.users.models import User
 
 
 class LoginView(TemplateView):
@@ -6,4 +7,5 @@ class LoginView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(LoginView, self).get_context_data(**kwargs)
+        context['users'] = User.objects.all()
         return context
