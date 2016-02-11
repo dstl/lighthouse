92f531c576804c734a15fc004f36148190c55e82
Dan Hough <daniel.hough@gmail.com>
2016-02-11 15:29:54 +0000

    Wrote a test to ensure link editing works properly

--- apps/links/tests.py
+++ apps/links/tests.py
@@ -36,3 +36,26 @@ class LinkTest(WebTest):
         self.assertEquals(form['description'].value,
                           'A great mapping application')
         self.assertEquals(form['destination'].value, 'https://wikimapia.org')
+
+    def test_edit_link_submit(self):
+        existing_link = Link(
+            name='Wikimapia',
+            description='A great mapping application',
+            destination='https://wikimapia.org')
+        existing_link.save()
+
+        form = self.app.get(
+            reverse('link-edit', kwargs={'pk': existing_link.pk})).form
+
+        form['name'].value = 'Bing Maps'
+        form['description'].value = 'Another great mapping application'
+        form['destination'].value = 'https://maps.bing.com'
+
+        response = form.submit().follow()
+
+        self.assertIn('Bing Maps', response)
+        self.assertNotIn('Wikimapia', response)
+        self.assertIn('Another great mapping application', response)
+        self.assertNotIn('A great mapping application', response)
+        self.assertIn('https://maps.bing.com', response)
+        self.assertNotIn('https://wikimapia.org', response)
