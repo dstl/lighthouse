6c0222e546f7b45131bb9d42df3847dff1310ddb
Michael Allen <michael@michaelallen.io>
2016-02-11 12:23:34 +0000

    Test the user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class User(models.Model):
     fullName = models.CharField(max_length=256)
-    phone = models.CharField(max_length=256)
-    email = models.CharField(max_length=256)
+    phone = models.CharField(max_length=256, blank=True, null=True)
+    email = models.CharField(max_length=256, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('user-detail', kwargs={'pk': self.pk})
---
+++ apps/users/tests.py
@@ -0,0 +1,34 @@
+from django.core.urlresolvers import reverse
+from django_webtest import WebTest
+
+
+class UserTest(WebTest):
+
+    def test_create_a_user_success(self):
+        form = self.app.get(reverse('user-create')).form
+        form['fullName'] = 'Jane Smith'
+        form['phone'] = '0 123 4567 890'
+        form['email'] = 'jane.smith@smithmail.com'
+
+        response = form.submit().follow()
+
+        self.assertEquals(response.status_int, 200)
+
+        user = response.context['user']
+
+        self.assertEquals(user.fullName, 'Jane Smith')
+        self.assertEquals(user.phone, '0 123 4567 890')
+        self.assertEquals(user.email, 'jane.smith@smithmail.com')
+
+    def test_create_a_user_fail(self):
+        form = self.app.get(reverse('user-create')).form
+        form['fullName'] = ''
+        form['phone'] = ''
+        form['email'] = ''
+
+        response = form.submit()
+        form = response.context['form']
+        self.assertIn(
+            "This field is required.",
+            form['fullName'].errors
+        )
