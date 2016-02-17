d6263e4a020e323a90a8b928c329242284df67a9
Dan Hough <daniel.hough@gmail.com>
2016-02-17 16:01:37 +0000

    Test & implement pre-checked boxes on edit page

---
+++ apps/links/tests/test_update_with_existing_categories.py
@@ -0,0 +1,50 @@
+from django.core.urlresolvers import reverse
+from apps.links.models import Link
+from apps.users.models import User
+
+from django_webtest import WebTest
+
+
+class CategorisedLinksWithCategoriesUpdateTest(WebTest):
+    def setUp(self):
+        self.logged_in_user = User(
+            fullName='Fake Fakerly',
+            phone='555-2187',
+            email='fake@dstl.gov.uk')
+        self.logged_in_user.save()
+
+        response = self.app.get(reverse('login-view'))
+
+        response = response.click('Fake Fakerly').follow()
+
+        self.assertEquals(response.html.h1.text, 'Fake Fakerly')
+
+        self.existing_link = Link(
+            name='Tweetbot',
+            description='A great twitter application',
+            destination='https://tweetbot.com',
+            owner=self.logged_in_user,)
+        self.existing_link.save()
+        self.existing_link.categories.add('social')
+        self.existing_link.categories.add('imagery')
+        self.existing_link.save()
+
+    def test_update_link_with_existing_categories_render(self):
+        form = self.app.get(
+            reverse('link-edit', kwargs={'pk': self.existing_link.pk})).form
+
+        self.assertTrue(form.get('categories', index=1).checked)
+        self.assertEquals(
+            form.html.findAll(
+                'label', {'class': 'link-category-label'}
+            )[0].text,
+            'Social'
+        )
+        self.assertTrue(form.get('categories', index=1).checked)
+        self.assertEquals(
+            form.html.findAll(
+                'label', {'class': 'link-category-label'}
+            )[1].text,
+            'Imagery'
+        )
+        self.assertEquals(form.get('categories', index=2).value, '')
