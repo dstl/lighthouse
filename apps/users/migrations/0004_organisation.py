7ed9f53fb7b9d54bf30d5ef484daa9c19ec33444
revdancatt <dan@revdancatt.com>
2016-02-24 14:25:38 +0000

    Make it lovelier!

--- apps/users/migrations/0004_organisation.py
+++ apps/users/migrations/0004_organisation.py
@@ -15,7 +15,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Organisation',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
                 ('name', models.CharField(max_length=256, unique=True)),
             ],
         ),
