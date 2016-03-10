b489c29172eb9cdbd88a42ba0b44afffdd0391c8
Dan Hough <daniel.hough@gmail.com>
2016-03-10 10:23:22 +0000

    Moved user tests into separate files based on what they do This is to avoid the painful merge conflict I got last time I did this as part of a larger piece of work.

---
+++ apps/users/tests/test_user_create.py
@@ -0,0 +1,38 @@
+# (c) Crown Owned Copyright, 2016. Dstl.
+# apps/users/tests.py
+from django.core.urlresolvers import reverse
+from django_webtest import WebTest
+
+
+class UserWebTest(WebTest):
+
+    def test_cannot_create_slugless_user(self):
+        form = self.app.get(reverse('login-view')).form
+        response = form.submit().follow().follow()
+        login_label = response.html.find(
+            'label',
+            attrs={'class': 'form-label-bold', 'for': 'id_slug'}
+        )
+        self.assertTrue(login_label)
+        self.assertEquals(login_label.text.strip(), 'Login with ID.')
+
+    def test_create_new_user(self):
+        #   got to the login form, and enter a user ID, this will sign us up.
+        form = self.app.get(reverse('login-view')).form
+        form['slug'] = 'user0001'
+        response = form.submit().follow()
+
+        #   Now go to the user profile page
+        response = self.app.get(
+            reverse(
+                'user-detail',
+                kwargs={'slug': 'user0001'}
+            )
+        )
+        #   Check that we have the user slug in the name in the nav bar
+        self.assertTrue(
+            response.html.find(
+                'span',
+                attrs={'data-slug': 'user0001'}
+            )
+        )
--- apps/users/tests/test_user_update.py
+++ apps/users/tests/test_user_update.py
@@ -2,122 +2,15 @@
 # apps/users/tests.py
 from django.core.urlresolvers import reverse
 from django_webtest import WebTest
-from django.test import TestCase
-from django.db.utils import IntegrityError
 
-from .models import User
+from apps.users.models import User
 from apps.teams.models import Team
 from apps.organisations.models import Organisation
 
 import re
 
 
-class UserTest(TestCase):
-    def setUp(self):
-        u = User(slug='user0001')
-        u.save()
-
-    def test_can_create_user(self):
-        u = User(slug='user0002')
-        u.save()
-        self.assertTrue(u.slug)
-
-    def test_cannot_create_duplicate_user(self):
-        u = User(slug='user0002')
-        u.save()
-        self.assertTrue(u.slug)
-        u = User(slug='user0001')
-        with self.assertRaises(IntegrityError):
-            u.save()
-
-    def test_user_can_have_multiple_teams_which_have_multiple_users(self):
-        o = Organisation(name='New Org')
-        o.save()
-
-        t1 = Team(name='Team Awesome', organisation=o)
-        t1.save()
-        t2 = Team(name='Team Great', organisation=o)
-        t2.save()
-
-        u1 = User(slug='teamplayer')
-        u1.save()
-
-        u1.teams.add(t1)
-        u1.teams.add(t2)
-        u1.save()
-
-        u2 = User(slug='teamplayer2')
-        u2.save()
-
-        u2.teams.add(t2)
-        u2.save()
-
-        self.assertIn(u1, t1.user_set.all())
-        self.assertIn(u1, t2.user_set.all())
-        self.assertNotIn(u2, t1.user_set.all())
-        self.assertIn(u2, t2.user_set.all())
-
-        self.assertEqual(len(t1.user_set.all()), 1)
-        self.assertEqual(len(t2.user_set.all()), 2)
-
-
 class UserWebTest(WebTest):
-
-    def test_cannot_create_slugless_user(self):
-        form = self.app.get(reverse('login-view')).form
-        response = form.submit().follow().follow()
-        login_label = response.html.find(
-            'label',
-            attrs={'class': 'form-label-bold', 'for': 'id_slug'}
-        )
-        self.assertTrue(login_label)
-        self.assertEquals(login_label.text.strip(), 'Login with ID.')
-
-    def test_create_new_user(self):
-        #   got to the login form, and enter a user ID, this will sign us up.
-        form = self.app.get(reverse('login-view')).form
-        form['slug'] = 'user0001'
-        response = form.submit().follow()
-
-        #   Now go to the user profile page
-        response = self.app.get(
-            reverse(
-                'user-detail',
-                kwargs={'slug': 'user0001'}
-            )
-        )
-        #   Check that we have the user slug in the name in the nav bar
-        self.assertTrue(
-            response.html.find(
-                'span',
-                attrs={'data-slug': 'user0001'}
-            )
-        )
-
-    def test_can_login_as_existing_user(self):
-        u = User(slug='user0001')
-        u.save()
-
-        #   Log in as user
-        form = self.app.get(reverse('login-view')).form
-        form['slug'] = 'user0001'
-        response = form.submit()
-
-        #   Now go to the user profile page
-        response = self.app.get(
-            reverse(
-                'user-detail',
-                kwargs={'slug': 'user0001'}
-            )
-        )
-        #   Check that we have the user slug in the name in the nav bar
-        self.assertTrue(
-            response.html.find(
-                'span',
-                attrs={'data-slug': 'user0001'}
-            )
-        )
-
     def test_update_button_shows_on_user_profile(self):
         #   Create the two users
         u1 = User(slug='user0001')
