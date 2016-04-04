b4cd277faa2de7bcc5a5c7a25532fe87d2fae547
Dan Hough <daniel.hough@gmail.com>
2016-04-04 16:39:51 +0100

    Combined Link List view with Search view
    Search now takes place on the Link List page.
    
    How does it work? Link List is still a ListView, but it imports and uses
    Haystack's SearchQuerySet and AutoQuery in order to do searches on the
    various fields which make up a Link/Tool.
    
    Name, categories, text (desc) and "externality" all are indexed. The
    only one which features custom behaviour is "externality". In order to
    make it text-searchable, i.e., that q=external would bring up links with
    external checked as true, a custom Haystack `SearchField` based on
    `CharField` was used, whose `convert` function was overridden to convert
    the boolean into text.

--- apps/links/search_indexes.py
+++ apps/links/search_indexes.py
@@ -5,9 +5,22 @@ from haystack import indexes
 from .models import Link
 
 
+class SearchableBooleanField(indexes.CharField):
+    def convert(self, value):
+        if value:
+            return 'external'
+        else:
+            return 'internal'
+
+
 class LinkIndex(indexes.SearchIndex, indexes.Indexable):
     name = indexes.CharField(model_attr='name')
+    categories = indexes.MultiValueField(indexed=True, stored=True)
     text = indexes.CharField(document=True, use_template=True)
+    network_location = SearchableBooleanField(model_attr='is_external')
 
     def get_model(self):
         return Link
+
+    def prepare_categories(self, link):
+        return [categories.name for categories in link.categories.all()]
