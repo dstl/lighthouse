18b09b3f090026e963e6522a0db3326796573497
Mark Norman Francis <norm@cackhanded.net>
2016-04-13 13:38:31 +0100

    Add missing models to the admin interface

---
+++ apps/links/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Link, LinkUsage
+
+
+class LinkUsageAdmin(admin.ModelAdmin):
+    readonly_fields = ('start', 'duration',)
+    exclude = ('end',)
+
+
+class LinkAdmin(admin.ModelAdmin):
+    readonly_fields = ('added',)
+    search_fields = ('name', 'description', 'destination',)
+
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(LinkUsage, LinkUsageAdmin)
