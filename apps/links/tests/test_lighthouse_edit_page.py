65d55dc251f5ed7f659e30f73fde6a8d25c27089
Dan Hough <daniel.hough@gmail.com>
2016-04-18 15:14:24 +0100

    API and Lighthouse now have no visible destination input.

---
+++ apps/links/tests/test_lighthouse_edit_page.py
@@ -0,0 +1,46 @@
+# (c) Crown Owned Copyright, 2016. Dstl.
+
+from django.core.urlresolvers import reverse
+
+from django_webtest import WebTest
+
+from testing.common import make_user, login_user, generate_fake_links
+
+
+class LighthouseGoToTest(WebTest):
+    def test_lighthouse_edit_page_has_no_destination_box(self):
+        self.logged_in_user = make_user()
+        self.app.get(reverse('login'))
+
+        self.assertTrue(login_user(self, self.logged_in_user))
+
+        response = self.app.get(reverse('link-edit', kwargs={'pk': 1}))
+
+        dest_input = response.html.find('input', {'name': 'destination'})
+
+        self.assertEqual(dest_input.attrs['type'], 'hidden')
+
+    def test_lighthouse_api_edit_page_has_no_destination_box(self):
+        self.logged_in_user = make_user()
+        self.app.get(reverse('login'))
+
+        self.assertTrue(login_user(self, self.logged_in_user))
+
+        response = self.app.get(reverse('link-edit', kwargs={'pk': 2}))
+
+        dest_input = response.html.find('input', {'name': 'destination'})
+
+        self.assertEqual(dest_input.attrs['type'], 'hidden')
+
+    def test_normal_edit_page_has_destination_box(self):
+        self.logged_in_user = make_user()
+        self.app.get(reverse('login'))
+        link, = generate_fake_links(owner=self.logged_in_user)
+
+        self.assertTrue(login_user(self, self.logged_in_user))
+
+        response = self.app.get(reverse('link-edit', kwargs={'pk': link.id}))
+
+        dest_input = response.html.find('input', {'name': 'destination'})
+
+        self.assertEqual(dest_input.attrs['type'], 'url')
