f59393d6ba0e9a35c1f820f3d9807588c54f84c0
Dan Hough <daniel.hough@gmail.com>
2016-04-26 16:00:18 +0100

    Correct API docs link

---
+++ apps/links/migrations/0007_modify_lighthouse_api_properly.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# (c) Crown Owned Copyright, 2016. Dstl.
+
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def modify_lighthouse_api_link_properly(apps, schema_editor):
+    # We can't import models directly from code as they will eventually
+    # be newer versions than this migration expects.
+    Link = apps.get_model('links', 'Link')
+
+    api_link = Link.objects.get(id=2)
+
+    description = 'The API for this application.'
+    description += '\n\nDocumentation for using the API can be found at '
+    description += '[/api/](/api/).'
+
+    api_link.description = description
+
+    api_link.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('links', '0006_modify_lighthouse_api'),
+    ]
+
+    operations = [
+        migrations.RunPython(modify_lighthouse_api_link_properly),
+    ]
