0e7f32b39de9340010f9644454166e9d058120af
Rich Brantingham <rich.brantingham@me.com>
2016-12-06 23:02:47 +0000

    New test to check that a user, on landing on home is recognised and redirected to /links

---
+++ apps/accounts/tests/test_user_landing.py
@@ -0,0 +1,13 @@
+# (c) Crown Owned Copyright, 2016. Dstl.
+
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+
+from django_webtest import WebTest
+
+class KeycloakHeaderLandAtHome(WebTest):
+    def test_auto_login(self):
+        headers = { 'KEYCLOAK_USERNAME' : 'user@0001.com' }
+        response = self.app.get(reverse('home'), headers=headers)
+        self.assertEqual(reverse('link-list'), response.location)
+
