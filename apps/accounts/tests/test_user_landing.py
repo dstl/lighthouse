78b14f269829b04f4c09fec3e39e148bc28b9699
Rich Brantingham <rich.brantingham@me.com>
2016-12-14 07:19:49 +0000

    Checking what comes through on the headers

--- apps/accounts/tests/test_user_landing.py
+++ apps/accounts/tests/test_user_landing.py
@@ -6,7 +6,7 @@ from django_webtest import WebTest
 
 class KeycloakHeaderLandAtHome(WebTest):
     def test_auto_login(self):
-        headers = { 'KEYCLOAK_USERNAME' : 'user@0001.com' }
+        headers = { 'KEYCLOAKUSERNAME' : 'user@0001.com' }
         response = self.app.get(reverse('home'), headers=headers)
         self.assertEqual(reverse('link-list'), response.location)
 
