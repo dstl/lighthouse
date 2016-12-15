e7b6ff3136866c75fc270cdc73b001414b6a250a
Rich Brantingham <rich.brantingham@me.com>
2016-12-15 15:10:32 +0000

    Reverting to previous home view behaviour.

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,10 +3,6 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.views.generic.base import View
-from django.contrib.auth import login
-from django.contrib.auth import get_user_model
-from django.conf import settings
-
 
 class Home(View):
 
@@ -15,18 +11,6 @@ class Home(View):
     #   check for the slug and either send them to the list of tools
     #   or bounce them to login.
     def get(self, request, *args, **kwargs):
-        userid = \
-            request.META.get(settings.KEYCLOAK_USERNAME_HEADER)
-        if userid:
-            try:
-                user = get_user_model().objects.get(userid=userid)
-            except:
-                user = get_user_model().objects.create_user(
-                    userid=userid, is_active=True)
-            user.backend = 'django.contrib.auth.backends.ModelBackend'
-            login(self.request, user)
-            self.user = user
-            return redirect(reverse('link-list'))
         try:
             u = request.user.slug
             if (u is not None and u is not ''):
